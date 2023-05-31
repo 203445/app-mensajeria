f30478fd61da37c8b26ca935b8aff0138d097de9
203445 <203445@ids.upchiapas.edu.mx>
2023-05-31 09:32:48 -0600

    add domain-entities users-chat

---
+++ app_mensajeria/lib/features/message/chat/domain/entities/chats.dart
@@ -0,0 +1,15 @@
+class Chats {
+  final String mensaje;
+  final String iduser;
+  final String imagen;
+  final String nombre;
+  final String archivo;
+
+  Chats({
+    required this.mensaje,
+    required this.iduser,
+    required this.imagen,
+    required this.nombre,
+    required this.archivo,
+  });
+}
---
+++ app_mensajeria/lib/features/message/users/domain/entities/users.dart
@@ -0,0 +1,15 @@
+class Users {
+  final String telefono;
+  final String informacion;
+  final String foto;
+  final String nombre;
+  final String id;
+
+  Users({
+    required this.telefono,
+    required this.informacion,
+    required this.foto,
+    required this.nombre,
+    required this.id,
+  });
+}
