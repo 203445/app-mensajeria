b8dba6528a9f0f5e07482a1a611ec65b431296c1
203445 <203445@ids.upchiapas.edu.mx>
2023-06-01 05:37:49 -0600

    delete files from users

--- app_mensajeria/lib/features/message/users/domain/repositories/chats_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:app_mensajeria/features/message/chat/domain/entities/chats.dart';
-
-abstract class ChatsRepository {
-  Future<List<Chats>> getChats();
-  Future<List<Chats>> deleteChats();
-  Future<List<Chats>> addChats();
-}
